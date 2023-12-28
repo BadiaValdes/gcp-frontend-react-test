ccd6a0b77c0f682cddd7b97166101951df327458
Reisel Valle <reiselvalle@gmail.com>
2023-12-28 09:36:49 -0500

    remove trash

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
   const [data, setData] = useState({
@@ -10,30 +8,20 @@ function App() {
   });
 
   useEffect(() => {
-    fetch("http://localhost:3001/").then(dat => dat.json()).then(d =>  {
-      console.log(d);
-      setData(d)
+    fetch("http://localhost:3001/")
+      .then(dat => dat.json())
+      .then(d =>  {
+        console.log(d);
+        setData(d)
     })
   },[])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Node + React + GCP</h1>
       <div className="card">
-        { data.name != '' && (<p>{data.name} - {data.last}</p>)   
-        }
+        { data.name != '' && (<p>{data.name} - {data.last}</p>) }
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
