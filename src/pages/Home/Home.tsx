a8b8e22fc42f916cc440d9240291b2df6cd1802b
emilio <jebv.informatico@gmail.com>
2024-01-09 17:58:08 -0500

    test

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,35 +1,31 @@
 import { useEffect, useState } from "react";
 import { GoogleAuth } from "google-auth-library";
+import axios from "axios";
 
 export default function Home() {
   const [user, setUser] = useState<string>("Aqui va el nombre");
 
- 
-
   useEffect(() => {
     const authFetch = async () => {
-      const auth = new GoogleAuth();
-      const authF = await auth.getIdTokenClient(
-        "https://gcp-backend-express-test-22vikqc2mq-uc.a.run.app"
-      );
-      const authHeader = await authF.getRequestHeaders();
-
       const serviceRequestOptions = {
-        method: 'GET',
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': authHeader['Authorization']
+          "Metadata-Flavor": "Google",
         },
-        timeout: 3000,
       };
-    
 
-      await fetch("https://gcp-backend-express-test-22vikqc2mq-uc.a.run.app", serviceRequestOptions)
-        .then((data) => data.json())
-        .then((json) => {
-          console.log(json);
-          setUser("Termino el fetch");
-        });
+      const metaData = await fetch(
+        "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?audience=https://gcp-backend-express-test-22vikqc2mq-uc.a.run.app",
+        serviceRequestOptions
+      );
+
+      console.log(metaData);
+
+      // await fetch("https://gcp-backend-express-test-22vikqc2mq-uc.a.run.app", serviceRequestOptions)
+      //   .then((data) => data.json())
+      //   .then((json) => {
+      //     console.log(json);
+      //     setUser("Termino el fetch");
+      //   });
     };
 
     authFetch();
