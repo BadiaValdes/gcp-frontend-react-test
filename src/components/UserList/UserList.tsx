94c10476fc9ea5b62760f80c3cbdebdb466f6d23
emilio <jebv.informatico@gmail.com>
2024-01-09 10:20:00 -0500

    fix: componentes, header and user list vite

--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -28,7 +28,7 @@ export default function UserList({ users, removeUser }:Props) {
             <td scope="row" className='p-1'>{user.email}</td>
             <td scope="row" className='actions-td'>
               <button type="button" aria-label="edit user"> <EditIcon/> </button>
-              <button type="button" onClick={()=>removeUser(user._id)} aria-label="remove user"> <RemoveIcon/> </button>
+              <button type="button" onClick={()=>removeUser(user._id as string)} aria-label="remove user"> <RemoveIcon/> </button>
             </td>
           </tr> 
         ))}            
