a2f3e5459562c911052be4976112d3487f76a8b4
emilio <jebv.informatico@gmail.com>
2024-01-09 15:19:50 -0500

    test sync

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   const [user, setUser]= useState<string>('Aqui va el nombre');
 
   useEffect(()=>{
-    fetch("https://gcp-backend-express-test-22vikqc2mq-uc.a.run.app/users").then(data => data.json()).then(json => {
+    fetch("https://gcp-backend-express-test-22vikqc2mq-uc.a.run.app").then(data => data.json()).then(json => {
       console.log(json)
       setUser('Termino el fetch');
     })
