dfd5f3517dd2da5f13b7751d6795f6a331bba5a7
emilio <jebv.informatico@gmail.com>
2024-01-09 14:51:01 -0500

    test sync

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,16 @@
+import { useEffect, useState } from "react"
 
 
 export default function Home() {
+  const [user, setUser]= useState<string>('Aqui va el nombre');
+
+  useEffect(()=>{
+    fetch("https://gcp-backend-express-test-22vikqc2mq-uc.a.run.app/users").then(data => data.json).then(json => {
+      console.log(json)
+    })
+  },[])
+
   return (
-    <div style={{color:'#fff'}}>Home</div>
+    <div style={{color:'#fff'}}>Home <span>{user}</span></div>
   )
 }
