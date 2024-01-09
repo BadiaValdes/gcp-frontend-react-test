2877fa8dda94205142bfd63aa0349fb93b7e2212
emilio <jebv.informatico@gmail.com>
2024-01-09 16:02:32 -0500

    test sync

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,32 @@
 import { useEffect, useState } from "react"
+const {GoogleAuth} = require('google-auth-library')
 
 
 export default function Home() {
   const [user, setUser]= useState<string>('Aqui va el nombre');
 
+  const auth = new GoogleAuth();
+
+  const clientId = '107158590866213564417';
+
   useEffect(()=>{
-    fetch("https://gcp-backend-express-test-22vikqc2mq-uc.a.run.app").then(data => data.json()).then(json => {
-      console.log(json)
-      setUser('Termino el fetch');
-    })
+    const authFetch = async () => {
+      const authF = await auth.getIdTokenClient('https://gcp-backend-express-test-22vikqc2mq-uc.a.run.app')
+      const authHeader = await authF.getRequestHeaders();
+    
+    
+
+      await fetch("https://gcp-backend-express-test-22vikqc2mq-uc.a.run.app", {
+        headers: new Headers({
+          'Authorization': authHeader
+        })
+      }).then(data => data.json()).then(json => {
+        console.log(json)
+        setUser('Termino el fetch');
+      })
+    }
+
+    authFetch();
   },[])
 
   return (
