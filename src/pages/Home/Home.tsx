631e52ab0cf4964be73fadc80c9a0e5f09fb164c
emilio <jebv.informatico@gmail.com>
2024-01-09 16:08:58 -0500

    test sync

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,33 +1,36 @@
-import { useEffect, useState } from "react"
-const {GoogleAuth} = require('google-auth-library')
-
+import { useEffect, useState } from "react";
+import { GoogleAuth } from "google-auth-library";
 
 export default function Home() {
-  const [user, setUser]= useState<string>('Aqui va el nombre');
+  const [user, setUser] = useState<string>("Aqui va el nombre");
 
   const auth = new GoogleAuth();
 
-  useEffect(()=>{
+  useEffect(() => {
     const authFetch = async () => {
-      const authF = await auth.getIdTokenClient('https://gcp-backend-express-test-22vikqc2mq-uc.a.run.app')
+      const authF = await auth.getIdTokenClient(
+        "https://gcp-backend-express-test-22vikqc2mq-uc.a.run.app"
+      );
       const authHeader = await authF.getRequestHeaders();
-    
-    
 
       await fetch("https://gcp-backend-express-test-22vikqc2mq-uc.a.run.app", {
         headers: new Headers({
-          'Authorization': authHeader['Authorization']
-        })
-      }).then(data => data.json()).then(json => {
-        console.log(json)
-        setUser('Termino el fetch');
+          Authorization: authHeader["Authorization"],
+        }),
       })
-    }
+        .then((data) => data.json())
+        .then((json) => {
+          console.log(json);
+          setUser("Termino el fetch");
+        });
+    };
 
     authFetch();
-  },[])
+  }, []);
 
   return (
-    <div style={{color:'#fff'}}>Home <span>{user}</span></div>
-  )
+    <div style={{ color: "#fff" }}>
+      Home <span>{user}</span>
+    </div>
+  );
 }
