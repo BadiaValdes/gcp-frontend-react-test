46ee58f0ef971cbef4e33670355b3ad6afc289b2
emilio <jebv.informatico@gmail.com>
2024-01-09 16:42:27 -0500

    fix

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,17 +8,17 @@ export default function Home() {
 
   useEffect(() => {
     const authFetch = async () => {
-      const auth = new GoogleAuth();
-      const authF = await auth.getIdTokenClient(
-        "https://gcp-backend-express-test-22vikqc2mq-uc.a.run.app"
-      );
-      const authHeader = await authF.getRequestHeaders();
+      // const auth = new GoogleAuth();
+      // const authF = await auth.getIdTokenClient(
+      //   "https://gcp-backend-express-test-22vikqc2mq-uc.a.run.app"
+      // );
+      // const authHeader = await authF.getRequestHeaders();
 
       const serviceRequestOptions = {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': authHeader["Authorization"]
+          'Authorization': ''
         },
         timeout: 3000,
       };
